7d962cde3ecb44f9a5fe9e732ad54f71e287dd9f
himza25 <samari.hamza25@gmail.com>
2023-06-21 03:17:31 -0700

    Updated task 16 - 98 Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,40 +8,40 @@
  */
 void fib_split(unsigned long a, unsigned long b, int n)
 {
-	int i;
-	unsigned long s1, s2, s3;
-	unsigned long p1, p2;
-	unsigned long t1, t2;
-	unsigned long carry = 0;
+        int i;
+        unsigned long s1, s2, s3;
+        unsigned long p1, p2;
+        unsigned long t1, t2;
+        unsigned long carry = 0;
 
-	p1 = a / 1000000000;
-	p2 = a % 1000000000;
-	s1 = b / 1000000000;
-	s2 = b % 1000000000;
+        p1 = a / 1000000000;
+        p2 = a % 1000000000;
+        s1 = b / 1000000000;
+        s2 = b % 1000000000;
 
-	for (i = 0; i < n; ++i)
-	{
-		t1 = s1;
-		t2 = s2;
-		s1 = p1;
-		s2 = p2;
-		if (t2 + s2 + carry > 999999999)
-		{
-			s3 = (t2 + s2 + carry) - 1000000000;
-			p1 = s1 + t1 + 1;
-		}
-		else
-		{
-			s3 = t2 + s2 + carry;
-			p1 = s1 + t1;
-		}
-		p2 = s3;
+        for (i = 0; i < n; ++i)
+        {
+                t1 = s1;
+                t2 = s2;
+                s1 = p1;
+                s2 = p2;
+                if (t2 + s2 + carry > 999999999)
+                {
+                        s3 = (t2 + s2 + carry) - 1000000000;
+                        p1 = s1 + t1 + 1;
+                }
+                else
+                {
+                        s3 = t2 + s2 + carry;
+                        p1 = s1 + t1;
+                }
+                p2 = s3;
 
-		carry = (s2 + t2) / 1000000000;
+                carry = (s2 + t2) / 1000000000;
 
-		printf(", %lu%09lu", p1, p2);
-	}
-	putchar('\n');
+                printf(", %lu%09lu", p1, p2);
+        }
+        putchar('\n');
 }
 
 /**
