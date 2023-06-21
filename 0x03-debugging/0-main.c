dd78f710206d661e19f68aefae41c495c609923f
himza25 <samari.hamza25@gmail.com>
2023-06-21 12:55:33 -0700

    Created test for positive_or_negative function

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-    int i;
+	int i;
 
-    i = 0;  // change this line from `i = 98` to `i = 0`
-    positive_or_negative(i);
+	i = 0;  // change this line from `i = 98` to `i = 0`
+	positive_or_negative(i);
 
-    return (0);
+	return (0);
 }
