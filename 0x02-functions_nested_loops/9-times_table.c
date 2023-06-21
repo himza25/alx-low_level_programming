19fa737c37e009a14d04da48d4b82d621e9c8627
himza25 <samari.hamza25@gmail.com>
2023-06-20 17:40:50 -0700

    Add tasks 4-11

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,6 +9,7 @@
 void times_table(void)
 {
 	int i, j, result;
+
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
@@ -16,7 +17,7 @@ void times_table(void)
 			result = i * j;
 			if (j != 0)
 			{
-			_putchar(','); 
+			_putchar(',');
 			_putchar(' ');
 				if (result < 10)
 					_putchar(' ');
