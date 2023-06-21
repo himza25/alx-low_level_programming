3886a23216fee1f317a11545a8aea4baa6bd3a80
himza25 <samari.hamza25@gmail.com>
2023-06-20 19:55:30 -0700

    Updated task 16 - 98 Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,41 +7,41 @@
  * @b: second part of large Fibonacci number
  * @n: Fibonacci sequence position
  */
-void fib_split(unsigned long a, unsigned long b, int n)
+void fib_split(unsigned int a, unsigned int b, int n)
 {
-	unsigned long s1, s2, s3;
-	unsigned long p1, p2;
-	unsigned long t1, t2;
-	unsigned long carry = 0;
+    unsigned int s1, s2, s3;
+    unsigned int p1, p2;
+    unsigned int t1, t2;
+    unsigned int carry = 0;
 
-	p1 = a / 1000000000;
-	p2 = a % 1000000000;
-	s1 = b / 1000000000;
-	s2 = b % 1000000000;
+    p1 = a / 1000000000;
+    p2 = a % 1000000000;
+    s1 = b / 1000000000;
+    s2 = b % 1000000000;
 
-	while (n-- > 0)
-	{
-		t1 = s1;
-		t2 = s2;
-		s1 = p1;
-		s2 = p2;
-		if (t2 + s2 + carry > 999999999)
-		{
-			s3 = (t2 + s2 + carry) - 1000000000;
-			p1 = s1 + t1 + 1;
-		}
-		else
-		{
-			s3 = t2 + s2 + carry;
-			p1 = s1 + t1;
-		}
-		p2 = s3;
+    while (n-- > 0)
+    {
+        t1 = s1;
+        t2 = s2;
+        s1 = p1;
+        s2 = p2;
+        if (t2 + s2 + carry > 999999999)
+        {
+            s3 = (t2 + s2 + carry) - 1000000000;
+            p1 = s1 + t1 + 1;
+        }
+        else
+        {
+            s3 = t2 + s2 + carry;
+            p1 = s1 + t1;
+        }
+        p2 = s3;
 
-		carry = (s2 + t2) / 1000000000;
+        carry = (s2 + t2) / 1000000000;
 
-		printf(", %lu, %09lu", p1, p2);
-	}
-	putchar('\n');
+        printf(", %u%010u", p1, p2);
+    }
+    putchar('\n');
 }
 
 /**
@@ -50,18 +50,18 @@ void fib_split(unsigned long a, unsigned long b, int n)
  */
 int main(void)
 {
-	unsigned long count, i = 0, j = 1, sum;
+    unsigned int count, i = 1, j = 2, sum;
 
-	printf("%lu, %lu", i, j);
-	for (count = 2; count < 98; count++)
-	{
-		sum = i + j;
-		printf(", %lu", sum);
-OAOAOAOA		i = j;
-		j = sum;
-OAOAOAOA	}
+    printf("%u, %u", i, j);
+    for (count = 2; count < 60; count++)
+    {
+        sum = i + j;
+        printf(", %u", sum);
+        i = j;
+        j = sum;
+    }
 
-	fib_split(i, j, 98 - 96);
+    fib_split(i, j, 98 - 60);
 
-OAOAOAOA	return 0;
-OAOAOAOA}
+    return (0);
+}
