2be83352b49d9ad68ac12ef75428233367af45e1
himza25 <samari.hamza25@gmail.com>
2023-06-21 03:26:21 -0700

    Updated task 16 - 98 Fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,74 +1,39 @@
 #include <stdio.h>
-
 /**
- * fib_split - split large Fibonacci numbers and print them
- * @a: first part of large Fibonacci number
- * @b: second part of large Fibonacci number
- * @n: Fibonacci sequence position
- */
-void fib_split(unsigned long a, unsigned long b, int n)
-{
-        int i;
-        unsigned long s1, s2, s3;
-        unsigned long p1, p2;
-        unsigned long t1, t2;
-        unsigned long carry = 0;
-
-        p1 = a / 1000000000;
-        p2 = a % 1000000000;
-        s1 = b / 1000000000;
-        s2 = b % 1000000000;
-
-        for (i = 0; i < n; ++i)
-        {
-                t1 = s1;
-                t2 = s2;
-                s1 = p1;
-                s2 = p2;
-                if (t2 + s2 + carry > 999999999)
-                {
-                        s3 = (t2 + s2 + carry) - 1000000000;
-                        p1 = s1 + t1 + 1;
-                }
-                else
-                {
-                        s3 = t2 + s2 + carry;
-                        p1 = s1 + t1;
-                }
-                p2 = s3;
-
-                carry = (s2 + t2) / 1000000000;
-
-                printf(", %lu%09lu", p1, p2);
-        }
-        putchar('\n');
-}
-
-/**
- * main - Entry point, prints first 98 Fibonacci numbers
- * Return: Always 0
- */
+* main - fibonacci <3
+*
+* Purpose - no hardcode
+* Return:  (Success)
+*/
 int main(void)
 {
-	int i;
-	unsigned long bef = 1;
-	unsigned long aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-
-	printf("%lu", bef);
-	for (i = 1; i < 91; i++)
-	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
-	}
-
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	
-	fib_split(bef1, bef2, 98 - 91);
-
-	return (0);
+unsigned long int i;
+unsigned long int a = 1;
+unsigned long int b = 2;
+unsigned long int l = 1000000000;
+unsigned long int a1;
+unsigned long int a2;
+unsigned long int b1;
+unsigned long int b2;
+printf("%lu", a);
+for (i = 1; i < 91; i++)
+{
+printf(", %lu", b);
+b += a;
+a = b - a;
+}
+a1 = (a / l);
+a2 = (a % l);
+b1 = (b / l);
+b2 = (b % l);
+for (i = 92; i < 99; ++i)
+{
+printf(", %lu", b1 + (b2 / l));
+printf("%lu", b2 % l);
+b1 = b1 + a1;
+a1 = b1 - a1;
+b2 = b2 + a2;
+a2 = b2 - a2;		}
+printf("\n");
+return (0);
 }
