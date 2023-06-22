c30ac9faaf6f6bb879ace020d116fbdf756a0d9e
himza25 <samari.hamza25@gmail.com>
2023-06-22 10:40:56 -0700

    Implemented the print_numbers function.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,15 +3,15 @@
 /**
  * print_numbers - prints numbers from 0 to 9 followed by a new line
  *
- * Return: Always 0 (success)
+ * * return: void
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 	}
 
 	_putchar('\n');
