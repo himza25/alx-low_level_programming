43f5a42a8e625ed1179076179c1690fb1179f90a
himza25 <samari.hamza25@gmail.com>
2023-06-22 11:29:24 -0700

    Implemented the function to print a triangle.

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,25 +7,25 @@
  */
 void print_triangle(int size)
 {
-    int i, j, space;
+	int i, j, space;
 
-    if (size > 0)
-    {
-        for (i = 0; i < size; i++)
-        {
-            for (space = size - i; space > 1; space--)
-            {
-                _putchar(' ');
-            }
-            for (j = 0; j <= i; j++)
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
-    else
-    {
-        _putchar('\n');
-    }
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (space = size - i; space > 1; space--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
 }
