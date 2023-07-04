1b20fbe60fb41bad892ccede0412c2316cf7bc01
himza25 <samari.hamza25@gmail.com>
2023-07-03 18:57:25 -0700

    Add solution for task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -5,7 +5,7 @@
  * @s: initial string.
  * @accept: substring.
  *
- * Return: the number of bytes in the initial segment of s which consist only of bytes from accept.
+ * Return: the number of bytes in the initial segment.
  */
 unsigned int _strspn(char *s, char *accept)
 {
