862cc193f8119836d0e3fa6f73211ac3fbe6231b
himza25 <samari.hamza25@gmail.com>
2023-07-03 18:50:51 -0700

    Add solution for task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,27 +1,21 @@
 #include "main.h"
 
 /**
- * _strchr - locates a character in a string
- * @s: the string to search
- * @c: the character to search for
+ * _strchr - locates a character in a string.
+ * @s: string to be scanned.
+ * @c: character to be searched in s.
  *
- * Return: pointer to first occurrence of c in s, or NULL if c is not found
+ * Return: pointer to the first occurrence of the character c in the string s,
+ * or NULL if the character is not found.
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
+	do {
 		if (*s == c)
 		{
 			return (s);
 		}
-		s++;
-	}
-
-	if (c == '\0')
-	{
-		return (s);
-	}
-
-	return (NULL);
+	} while (*s++);
+	
+	return (0);
 }
