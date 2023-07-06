bdfb881bb288e7a7a4a04cf770f4b6637ee7013e
himza25 <samari.hamza25@gmail.com>
2023-07-05 19:14:11 -0700

    Add solution for advanced task 101

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares two strings and returns 1 if the strings
+ * can be considered identical, otherwise returns 0.
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: 1 if the strings are identical, otherwise returns 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
+	return (0);
+}
