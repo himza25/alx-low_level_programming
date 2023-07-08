33d456c919d25aa841770e4bcf277f3b50e31171
himza25 <samari.hamza25@gmail.com>
2023-07-07 18:26:25 -0700

    Added solution for task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -8,11 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	/* To avoid compiler warning of unused variable */
 	(void)argv;
-
-	/* Since the first argument is always the program name, 
-	the number of arguments passed into the program will be argc - 1 */
 	printf("%d\n", argc - 1);
 
 	return (0);
