23bb4a16733556f3aca40abdc60f53447feeb767
himza25 <samari.hamza25@gmail.com>
2023-07-13 12:01:34 -0700

    Add a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,87 +1,130 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
 
 /**
- * _isdigit - check if the string is a number
- * @s: string to check
- * Return: 1 if is a number otherwise 0
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
-int _isdigit(char *s)
+int is_digit(char *s)
 {
-	while (*s)
-		if (*s < '0' || *s > '9')
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-	else
-			s++;
+		i++;
+	}
 	return (1);
 }
 
 /**
- * print_number - print the given number
- * @s: string to print
- * @len: length of the string to print
- * Return: void
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
  */
-void print_number(char *s, int len)
+int _strlen(char *s)
 {
-	while (len--)
-		_putchar(*s++ + '0');
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * multiplication_logic - performs the actual multiplication logic
+ * @s1: first number as a string
+ * @s2: second number as a string
+ * 
+ * Return: pointer to an array with the result
+ */
+int *multiplication_logic(char *s1, char *s2)
+{
+	int len1, len2, len, i, carry, digit1, digit2, *result;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		exit(98);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+	return result;
+}
+
+/**
+ * print_product - prints the product of the multiplication
+ * @result: result of the multiplication
+ * @len: length of the result
+ */
+void print_product(int *result, int len)
+{
+	int i, a = 0;
+	for (i = 0; i < len - 1; i++)
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
 }
 
 /**
- * mul - multiplies two numbers
- * @s1: first number
- * @s2: second number
- * Return: pointer to a buffer that contains the result
- */
-int *mul(char *s1, char *s2)
-{
-	int l1, l2, a, b, *res;
-
-	for (l1 = 0; s1[l1]; l1++)
-		s1[l1] -= '0';
-	for (l2 = 0; s2[l2]; l2++)
-		s2[l2] -= '0';
-	res = malloc(l1 + l2);
-	if (!res)
-		exit(98);
-	for (b = l2 - 1; b >= 0; b--)
-		for (a = l1 - 1; a >= 0; a--)
-			res[a + b + 1] += s2[b] * s1[a];
-	for (a = l1 + l2 - 1; a > 0; a--)
-	{
-		res[a - 1] += res[a] / 10;
-		res[a] %= 10;
-	}
-	return (res);
-}
-
-/**
- * main - Entry point
+ * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success, otherwise 98
+ *
+ * Return: always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int *res;
-	int l1, l2;
+	char *s1, *s2;
+	int *result, len;
 
-	if (argc != 3 || !_isdigit(argv[1]) || !_isdigit(argv[2]))
-	{
-		puts("Error");
-		exit(98);
-	}
-	l1 = 0;
-	while (argv[1][l1])
-		l1++;
-	l2 = 0;
-	while (argv[2][l2])
-		l2++;
-	res = mul(argv[1], argv[2]);
-	if (res[0] == 0)
-		l1--;
-	print_number(res + l1 + l2 - 1, l1 + l2);
-	free(res);
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
+		errors();
+	s1 = argv[1];
+	s2 = argv[2];
+	result = multiplication_logic(s1, s2);
+	len = _strlen(s1) + _strlen(s2) + 1;
+	print_product(result, len);
+	free(result);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -11,8 +11,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int _isdigit(char *s);
-void print_number(char *s, int len);
-int *mul(char *s1, char *s2);
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int *multiplication_logic(char *s1, char *s2);
+void print_product(int *result, int len);
 
 #endif /* MAIN_H */
