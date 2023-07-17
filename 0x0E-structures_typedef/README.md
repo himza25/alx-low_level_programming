6b029ad4751f732203c70d99a74cce5c3b463a99
himza25 <samari.hamza25@gmail.com>
2023-07-17 11:55:57 -0700

    Add a new structure type struct dog with the elements: name, age, and owner

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Task 0 : This program includes a new structure type struct dog with the elements: name, age, and owner.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description of the structure
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
