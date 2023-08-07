f7229c88471c80cc201edb33b64f0d921b1a0ad1
himza25 <himza.of.theking95@gmail.com>
2023-08-07 07:31:28 -0700

    Completed task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Appends text at the end of a file.
+ * @filename: Name of the file.
+ * @text_content: Content to be appended.
+ * Return: 1 on success, -1 on failure.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, write_length, i;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	for (i = 0; text_content[i]; i++)
+		;
+
+	write_length = write(fd, text_content, i);
+	close(fd);
+
+	if (write_length == -1)
+		return (-1);
+
+	return (1);
+}
--- 0x15-file_io/README.md
+++ 0x15-file_io/README.md
@@ -1,2 +1,3 @@
 Task 0 : This project is about file I/O in C. We are implementing various functions to handle reading and writing to files.
 Task 1: This task involves creating a new file and writing content to it.
+Task 2: This task involves appending content to the end of a file.
