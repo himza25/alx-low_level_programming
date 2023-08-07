51d312d648443ec7d52cd86283f602854c6aa74a
himza25 <himza.of.theking95@gmail.com>
2023-08-07 07:21:00 -0700

    Completed task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,20 +13,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	/* Open for writing. If file doesn't exist, create it with 0600 permissions.
-	   If it exists, truncate it. */
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	// If there's no content to write, just close the file and return.
 	if (!text_content)
 	{
 		close(fd);
 		return (1);
 	}
 
-	// Get the length of text_content.
 	for (i = 0; text_content[i]; i++)
 		;
 
