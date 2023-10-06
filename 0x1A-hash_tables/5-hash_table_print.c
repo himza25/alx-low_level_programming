446f4c4640707fbf635ef55b3f9df22e4dafa200
himza25 <himza.of.theking95@gmail.com>
2023-10-05 18:50:35 -0700

    completed task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,7 +6,7 @@ int check_hash_table(hash_node_t **hash, unsigned long int size);
 /**
  * hash_table_print - prints a hash table.
  * @ht: the hash table
-*/
+ */
 void hash_table_print(const hash_table_t *ht)
 {
 	int i = 0, check, prchick = 0;
@@ -37,7 +37,7 @@ void hash_table_print(const hash_table_t *ht)
  * @hash: the array
  * @size: size of array
  * Return: the number of elements
-*/
+ */
 int check_hash_table(hash_node_t **hash, unsigned long int size)
 {
 	int check = 0, i = 0;
