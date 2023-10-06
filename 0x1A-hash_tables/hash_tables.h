f30d52d5b568dc38fb073dbe9b2262d51baa3275
himza25 <himza.of.theking95@gmail.com>
2023-10-05 18:23:18 -0700

    Implement hash_table_create function

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,6 +40,7 @@ unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 hash_node_t *add_node_hash(hash_node_t **head, char *key, char *value);
 
 char *hash_table_get(const hash_table_t *ht, const char *key);
@@ -48,4 +49,4 @@ void hash_table_print(const hash_table_t *ht);
 
 void hash_table_delete(hash_table_t *ht);
 
-#endif
+#endif /* HASH_TABLES_H */
