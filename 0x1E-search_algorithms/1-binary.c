aff678bd85ae53221ca4f40a9dba950fba0f5d4f
himza25 <himza.of.theking95@gmail.com>
2024-01-15 15:42:35 -0800

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -3,13 +3,15 @@
 /**
  * print_array - Prints an array of integers
  * @array: The array to be printed
- * @left: Left index of the array to print
- * @right: Right index of the array to print
+ * @left: The starting index of the subarray to print
+ * @right: The ending index of the subarray to print
  */
-void print_array(int *array, size_t left, size_t right)
+void print_array(const int *array, size_t left, size_t right)
 {
+	size_t i;
+
 	printf("Searching in array: ");
-	for (size_t i = left; i <= right; i++)
+	for (i = left; i <= right; i++)
 	{
 		printf("%d", array[i]);
 		if (i < right)
@@ -38,12 +40,12 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		print_array(array, left, right);
 		mid = left + (right - left) / 2;
+		print_array(array, left, right);
 
 		if (array[mid] == value)
 			return (mid);
-		if (array[mid] < value)
+		else if (array[mid] < value)
 			left = mid + 1;
 		else
 			right = mid - 1;
