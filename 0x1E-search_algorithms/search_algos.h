704305a33468e6493cfea1f7053d627f330b561e
himza25 <himza.of.theking95@gmail.com>
2024-01-17 15:04:43 -0800

    update header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -38,14 +38,14 @@ typedef struct skiplist_s
     struct skiplist_s *express;
 } skiplist_t;
 
-
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(skiplist_t *list, int value);
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value); /* Linear search algorithm */
+int binary_search(int *array, size_t size, int value); /* Binary search algorithm */
+int jump_search(int *array, size_t size, int value); /* Jump search algorithm */
+int interpolation_search(int *array, size_t size, int value); /* Interpolation search algorithm */
+int exponential_search(int *array, size_t size, int value); /* Exponential search algorithm */
+int advanced_binary(int *array, size_t size, int value); /* Advanced binary search algorithm */
+listint_t *jump_list(listint_t *list, size_t size, int value); /* Jump search in a singly linked list */
+skiplist_t *linear_skip(skiplist_t *list, int value); /* Linear search in a skip list */
 
 #endif /* SEARCH_ALGOS_H */
