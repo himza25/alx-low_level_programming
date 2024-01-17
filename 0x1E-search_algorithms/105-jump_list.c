c7d7fe5c53cc1685b9a276c34c862557e5e2af34
himza25 <himza.of.theking95@gmail.com>
2024-01-17 15:44:50 -0800

    task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,44 +1,50 @@
 #include "search_algos.h"
+#include <math.h>
 
 /**
- * jump_list - Searches for a value in a sorted singly linked list of integers
- *             using the Jump search algorithm.
- * @list: A pointer to the head of the singly linked list.
- * @size: The number of nodes in the list.
- * @value: The value to search for.
+ * jump_list - searches for a value in an array of
+ * integers using the Jump search algorithm
  *
- * Return: A pointer to the first node where value is located, or NULL if value
- *         is not present or list is NULL.
+ * @list: input list
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
-	size_t jump_step = (size_t)sqrt(size);
-	size_t step;
-	listint_t *prev = list, *current = list;
+	size_t index, k, m;
+	listint_t *prev;
 
-	if (!list)
+	if (list == NULL || size == 0)
 		return (NULL);
 
-	/* Jumping through the list */
-	while (current->index < size && current->n < value)
-	{
-		prev = current;
-		for (step = 0; step < jump_step && current->next; step++)
-			current = current->next;
+	m = (size_t)sqrt((double)size);
+	index = 0;
+	k = 0;
 
-		printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-	}
+	do {
+		prev = list;
+		k++;
+		index = k * m;
 
-	/* Linear search in the sublist */
-	printf("Value found between indexes [%lu] and [%lu]\n",
-			prev->index, current->index);
-	while (prev && prev->index <= current->index)
+		while (list->next && list->index < index)
+			list = list->next;
+
+		if (list->next == NULL && index != list->index)
+			index = list->index;
+
+		printf("Value checked at index [%d] = [%d]\n", (int)index, list->n);
+
+	} while (index < size && list->next && list->n < value);
+
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)prev->index, (int)list->index);
+
+	for (; prev && prev->index <= list->index; prev = prev->next)
 	{
-		printf("Value checked at index [%lu] = [%d]\n", prev->index, prev->n);
+		printf("Value checked at index [%d] = [%d]\n", (int)prev->index, prev->n);
 		if (prev->n == value)
 			return (prev);
-
-		prev = prev->next;
 	}
 
 	return (NULL);
